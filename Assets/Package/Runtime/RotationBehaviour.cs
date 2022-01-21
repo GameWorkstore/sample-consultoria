67135418951e94311cc4d9a2170e9dd5f6fab568
HanzaRu <woodimbr@gmail.com>
2022-01-21 10:41:03 -0300

    +Version 0.1.1

--- Assets/Package/Runtime/RotationBehaviour.cs
+++ Assets/Package/Runtime/RotationBehaviour.cs
@@ -1,16 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace SampleModule
 {
+	public enum Axis
+	{
+		X,
+		Y,
+		Z
+	}
+
 	public class RotationBehaviour : MonoBehaviour
 	{
 		public float RotationRate = 30;
+		public Axis Axis;
 
 		public void Update()
 		{
-			transform.Rotate(RotationRate * Time.deltaTime, 0, 0);
+			switch (Axis)
+			{
+				case Axis.X:
+					transform.Rotate(RotationRate * Time.deltaTime, 0, 0);
+					break;
+				case Axis.Y:
+					transform.Rotate(0,RotationRate * Time.deltaTime, 0);
+					break;
+				case Axis.Z:
+					transform.Rotate(0,0,RotationRate * Time.deltaTime);
+					break;
+			}
 		}
 
 	}
